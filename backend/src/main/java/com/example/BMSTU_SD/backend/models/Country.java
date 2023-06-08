16b8783537deb20230d1fe6b5b61b489be96e458
katrinn777 <kondratieva227@yandex.ru>
2023-06-08 22:33:31 +0300

    9-12 лабы

---
+++ backend/src/main/java/com/example/BMSTU_SD/backend/models/Country.java
@@ -0,0 +1,55 @@
+package com.example.BMSTU_SD.backend.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "countries")
+@Access(AccessType.FIELD)
+public class Country {
+
+    public Country() { }
+    public Country(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    public long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    public String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Artist> getArtists() {
+        return artists;
+    }
+
+    public void setArtists(List<Artist> artists) {
+        this.artists = artists;
+    }
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "country")
+    public List<Artist> artists = new ArrayList<Artist>();
+
+}
